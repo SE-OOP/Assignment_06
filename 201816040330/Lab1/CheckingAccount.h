96065e11c0a4e8ebefa26458fcbaf70b0cf258a0
201816040330 <56644045+201816040330@users.noreply.github.com>
2019-12-08 15:46:44 +0800

    Create CheckingAccount.h

---
+++ 201816040330/Lab1/CheckingAccount.h
@@ -0,0 +1,40 @@
+// Lab 1: CheckingAccount.h
+// Definition of CheckingAccount class.
+#ifndef CHECKING_H
+#define CHECKING_H
+
+#include "Account.h"/* Write a directive to include the Account header file */
+
+class CheckingAccount: public Account/* Write a line to have class CheckingAccount inherit publicly from Account */
+{
+public:
+   // constructor initializes balance and transaction fee
+   explicit CheckingAccount(double,double);/* Declare a two-argument constructor for CheckingAccount */
+
+   void credit( double );
+   bool debit( double ); /* Redeclare member function credit, which will be redefined */
+   /* Redeclare member function debit, which will be redefined */
+private:
+   double fee;/* Declare data member transactionFee */
+
+   // utility function to charge fee
+   void chargeFee();/* Declare member function chargeFee */
+}; // end class CheckingAccount
+
+#endif
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
