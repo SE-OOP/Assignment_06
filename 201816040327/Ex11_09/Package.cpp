93dbf2bb8eb08ef12551cd7698c03476e131dbdb
MisonoMayuBW <44313792+MisonoMayuBW@users.noreply.github.com>
2019-12-08 17:43:49 +0800

    Create Package.cpp

---
+++ 201816040327/Ex11_09/Package.cpp
@@ -0,0 +1,106 @@
+//
+//  Package.cpp
+//  Ex11_09
+//
+//  Created by misonomayubw on 2019/12/8.
+//  Copyright © 2019 misonomayubw. All rights reserved.
+//
+
+#include <iostream>
+#include "Package.h"
+using namespace std;
+
+Package :: Package(
+                   const string &sn,const string &rn, const string &ra, const string &rp, const string &rc, const string &rz,
+                   double weight, double fe)
+                   {
+                       senderName (sn) ;
+
+                       recipientName (rn) ;
+                       recipientAddress (ra) ;
+                       recipientProvince (rp);
+                       recipientCity  (rc);
+                       recipientZip (rz);
+                       setPackageweight(weight);
+                       setFee(fe);
+                   }
+
+void Package :: setSenderName(const string &sn)
+{
+    senderName = sn;
+}
+string Package ::getSenderName()const
+{
+    return senderName;
+}
+
+void Package :: setRecipientName(const string &rn)
+{
+    recipientName = rn;
+}
+string Package ::getRecipientName()const
+{
+    return recipientName;
+}
+void Package :: setRecipientAddress(const string &ra)
+{
+    recipientAddress = ra;
+}
+string Package :: getRecipientAddress()const
+{
+    return recipientAddress;
+}
+void Package :: setRecipientProvince(const string &rp)
+{
+    recipientProvince = rp;
+}
+string Package :: getRecipientProvince()const
+{
+    return recipientProvince;
+}
+void Package :: setRecipientCity (const string &rc)
+{
+    recipientCity = rc;
+}
+string Package :: getRecipientCity()const
+{
+    return recipientCity;
+}
+void Package :: setRecipientZip(const string &rz)
+{
+   recipientZip = rz;
+}
+string Package :: getRecipientZip()const
+{
+    return recipientZip;
+}
+void Package :: setPackageWeight(double weight)
+{
+    if(weight > 0.0)
+        packageWeight = weight;
+    else
+        throw invalid_argument("Package weight must be >0.0");
+}
+double Package ::getPackageweight()const
+{
+    return packageWeight;
+}
+void Package :: setFee(double fe)
+{
+    if(fe >0)
+         fee = fe;
+     else
+     {
+         cout << "cost<=0" << endl;
+         cout << "Fee must be >0.0" << endl;
+         exit(0);
+     }
+}
+double Package :: getFee()const
+{
+    return fee;
+}
+double Package :: calculateConst()const
+{
+    return packageWeight * fee;
+}
